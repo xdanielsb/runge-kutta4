74aac2869c84a5b689a12a29f5da4d9bc94a58ab
Daniel <dfsantosbu@unal.edu.co>
2020-10-20 09:11:36 +0200

    Separate responsabilities, move the exact solution.

--- main.py
+++ main.py
@@ -14,31 +14,30 @@ class RungeKutta:
         self.avg = []
         self.h = h
 
-    def exact_solution(self, t):
-        # this is the solution given by the average equations
-        # of the vanderpol system 
-        ep = 0.1
-        t = (t-1)*self.h
-        ans = 2.0 / math.sqrt ( 3* (math.e ** (-t*ep)) + 1)
-        ans = ans * math.cos(t)
-        return ans
-
-
-    def runge(self, f, x, y):
+    def runge(self, f1, f2, x, y):
         for it in range(self.iters):
             self.lx.append(x)
             self.ly.append(y)
-            self.avg.append( self.exact_solution(it+5))
-            k1, m1 = f( x, y, self.h )
-            k2, m2 = f( x + k1/2, y + m1/2, self.h)
-            k3, m3 = f( x + k2/2, y + m2/2, self.h)
-            k4, m4 = f( x + k3,  y + m3, self.h)
+            self.avg.append( f2(it+5, self.h))
+            k1, m1 = f1( x, y, self.h )
+            k2, m2 = f1( x + k1/2, y + m1/2, self.h)
+            k3, m3 = f1( x + k2/2, y + m2/2, self.h)
+            k4, m4 = f1( x + k3,  y + m3, self.h)
             x = x + ( k1 + 2*k2 + 2*k3 + k4) / 6.0
             y = y + ( m1 + 2*m2 + 2*m3 + m4) / 6.0
-        fr = 0
+        fr = 200
         plt.plot(self.lx[fr:], self.ly[fr:], 'g:', self.lx[fr:], self.avg[fr:], 'b-')
         plt.show()
 
+
+def exact_solution(t, h):
+    # this is the solution given by the average equations
+    # of the vanderpol system 
+    ep = 0.1
+    t = (t-1)*h
+    ans = 2.0 / math.sqrt ( 3* (math.e ** (-t*ep)) + 1)
+    ans = ans * math.cos(t)
+    return ans
 
 
 def vanderpol(x, y, h):
@@ -51,5 +50,5 @@ def vanderpol(x, y, h):
 
 if __name__ == "__main__":
     obj = RungeKutta()
-    obj.runge(vanderpol, x=1, y=2)
+    obj.runge(vanderpol, exact_solution, x=1, y=2)
     
